9d634011af24057a670c8f4edfd8507b600a6ef9
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2022-07-07 17:20:55 +0200

    Adding colors to the terminal output.

--- Utility.py
+++ Utility.py
@@ -1,10 +1,30 @@
 import AVMerger as avm
-import os, re
+from rich.console import Console
+from rich.theme import Theme
+
 import youtube_dl
+import os, re
+
 # Available functions in this module:
 # ['format_name',    'yes_no_choice',                    'check_stream_existence',
 #  'merge_streams',  'optional_downloads',               'download_subtitles',
 #  'select_streams', 'format_selected_stream_into_dict', 'download_streams']
+
+
+
+custom_theme = Theme({
+    "normal1"       :   "bold blue1 on grey23",
+    "normal2"       :   "bold dark_violet",
+    
+    "warning1"      :   "bold plum4 on grey23",
+    "warning2"      :   "bold red",
+    
+    "exists"        :   "bold chartreuse3 on gray23"
+})
+
+console = Console(theme=custom_theme)
+
+
 
 def format_name(filename, pattern, replacement=""):
     new_named    = re.sub(pattern, replacement, filename)
@@ -13,10 +33,11 @@ def format_name(filename, pattern, replacement=""):
 
 
 
-def yes_no_choice(message, blank_true=False):
-    choice = input(message).lower()
-    print("")
-
+def yes_no_choice(blank_true=False):
+    choice = input().lower()
+    console.print("")
+    
+    # blank_true >> return True if the user didn't enter anything
     if choice in ["1", "yes", "y"] or not choice and blank_true:
         return True
     if choice in ["-1", "skip"]:
@@ -28,26 +49,27 @@ def yes_no_choice(message, blank_true=False):
 def check_stream_existence(stream_obj, formated_filename, valid_filename, stream_type):
     check1 = os.path.isfile(valid_filename + '.mp4')
     check2 = os.path.isfile(formated_filename + (" (Video).mp4" if stream_type == "video" else " (Audio).mp4"))
+    
     if not check1 and not check2:
         stream_obj.download()
         os.rename(valid_filename + ".mp4", formated_filename + (" (Video).mp4" if stream_type == "video" else " (Audio).mp4"))
     elif check1:
         os.rename(valid_filename + ".mp4", formated_filename + (" (Video).mp4" if stream_type == "video" else " (Audio).mp4"))
     else:
-        print(f"{'Video' if stream_type == 'video' else 'Audio'} stream already downloaded\n")
+        console.print(f"[exists][normal2]{'Video' if stream_type == 'video' else 'Audio'}[/] stream already downloaded[/]\n")
 
 
 
 def merge_streams(formated_filename, vid_id):
-    print("Starting merging...")
+    console.print("[bold blue1 on grey23]Starting merging...[/]")
     subtitles = download_subtitles(vid_id, formated_filename)
     try:
         avm.avmerger(   os.path.dirname(os.path.abspath(__file__)),
                         formated_filename, subtitles)
     except:
-        print("Something went wrong! Check if both the video and audio streams have been downloaded correctly.\n")
+        console.print("[warning1]Something went wrong! Check if both the [warning2]video[/] and [warning2]audio[/] streams have been downloaded correctly.[/]\n")
     else:
-        print(f"File merged successfully.\n")
+        console.print(f"[normal1]File merged successfully.[/]\n")
 
 
 
@@ -58,17 +80,17 @@ def optional_downloads(formated_filename, option_name, vid_obj):
                 if option_file == "Description":
                     option_file.write(vid_obj.description)
         except:
-            print(f"Sorry, could't download the video {option_name}!\n")
+            console.print(f"[warning1]Sorry, couldn't download the [warning2]video {option_name}[/]![/]\n")
         else:
-            print("")
+            console.print("")
     else:
-        print(f"Video {option_name.lower()} already downloaded\n")
+        console.print(f"[exists][normal2]Video {option_name.lower()}[/] already downloaded[/]\n")
 
 
 
 def download_subtitles(vid_id, formated_filename):
     if not os.path.isfile(formated_filename + ".en.vtt") and not os.path.isfile(formated_filename + ".ar.vtt"):
-        print("Downloading subtitles...")
+        console.print("[normal1]Downloading [normal2]subtitles[/]...[/]")
         
         ## useful links: https://github.com/ytdl-org/youtube-dl#subtitle-options
         #                https://hassanlatif.net/how-to-download-youtube-videos-with-subtitles-using-youtube-dl-646.html
@@ -86,7 +108,7 @@ def download_subtitles(vid_id, formated_filename):
 
         os.system(f"youtube-dl --write-sub --write-auto-sub --sub-lang ar,en --skip-download \"https://www.youtube.com/watch?v={vid_id}\" -o \"{formated_filename}\"")
     else:
-        print("Subtitles already downloaded.\n")
+        console.print("[exists][normal2]Subtitles[/] already downloaded.[/]\n")
     
     subtitles = ""
     if os.path.isfile(formated_filename + ".en.vtt"):
@@ -96,21 +118,27 @@ def download_subtitles(vid_id, formated_filename):
     return subtitles
 
 
-def select_streams(merge_option, categories_lengths, message):
+def select_streams(merge_option, categories_lengths, skip_not_stop = True):
     valid_choices = False
+    if merge_option:
+        message = f"[normal1]Select a [normal2]category[/] and a [normal2]resolution[/] option for both the video & audio streams ([normal2]4[/] numbers) separated by [normal2]spaces[/] or [normal2]leave empty[/] to {'skip' if skip_not_stop else 'stop'}: [/]"
+    else:
+        message = f"[normal1]Select a [normal2]category[/] and a [normal2]resolution[/] option separated by a [normal2]space[/] or [normal2]leave empty[/] to {'skip' if skip_not_stop else 'stop'}: [/]"
+
     while not valid_choices:
-        choices = input(message).split(" ")
-        print("")
+        console.print(message, end='')
+        choices = input().split(" ")
+        console.print("")
 
         # If choices is empty return ['']
         if len(choices) == 1 and not choices[0]:
                 return choices
 
         if len(choices) > (4 if merge_option else 2):
-            print(f"Error. Invalid input. Requested {4 if merge_option else 2} numbers, but got {len(choices)}. Your input: {choices}.\n")
+            console.print(f"[warning1]Invalid input. Requested [warning2]{4 if merge_option else 2}[/] numbers, but got [warning2]{len(choices)}[/]. Your input: [warning2]{choices}[/][/]\n")
             continue
         elif len(choices) != (4 if merge_option else 2):
-            print(f"Error. Invalid input. Not enough data. Requested {4 if merge_option else 2} numbers, but got {len(choices)}. Your input: {choices}.\n")
+            console.print(f"[warning1][warning2]Not enough data[/]. Requested [warning2]{4 if merge_option else 2}[/] numbers, but got [warning2]{len(choices)}[/]. Your input: [warning2]{choices}[/][/]\n")
             continue
         
         try:
@@ -118,17 +146,17 @@ def select_streams(merge_option, categories_lengths, message):
                 if merge_option and int(choices[2]) <= categories_lengths[0] or not merge_option:
                     for i in range(1, len(choices), 2):
                         if int(choices[i]) > categories_lengths[int(choices[i-1])]:
-                            print(int(choices[i]), categories_lengths[int(choices[i-1])])
-                            print(f"Error Encountered. Make sure the selected stream number{'s are' if merge_option else ' is'} correct. Your input: {choices}.\n")
+                            console.print(int(choices[i]), categories_lengths[int(choices[i-1])])
+                            console.print(f"[warning1][warning2]Error Encountered[/]. Make sure the [warning2]selected stream number{'s[/] are' if merge_option else '[/] is'} correct. Your input: [warning2]{choices}[/][/]\n")
                             break
                     else:
                         valid_choices = True
                 elif merge_option and int(choices[2]) > categories_lengths[0]:
-                    print(f"Error Encountered. Make sure the second selected category number is correct. Your input: {choices}.\n")
+                    console.print(f"[warning1][warning2]Error Encountered[/]. Make sure the [warning2]second selected category number[/] is correct. Your input: [warning2]{choices}[/][/]\n")
             else:
-                print(f"Error Encountered. Make sure the{' first' if merge_option else ''} selected category number is correct. Your input: {choices}.\n")
+                console.print(f"[warning1][warning2]Error Encountered[/]. Make sure the [warning2]{'first' if merge_option else ''} selected category number[/] is correct. Your input: [warning2]{choices}[/][/]\n")
         except:
-            print(f"Invalid input. You have entered something wrong. Your input: {choices}.\n")
+            console.print(f"[warning1]Invalid input. You have entered something wrong. Your input: [warning2]{choices}[/][/]\n")
     return choices
 
 
@@ -158,15 +186,15 @@ def format_selected_stream_into_dict(formated_filename, valid_filename, selected
 def download_streams(selected_streams):
     for stream in selected_streams:
         if 'video' in stream:
-            print(f"Downloading {stream['video'][0][0].title}")
-            print(f"{stream['video'][0][1]}, {stream['video'][0][-4]}, {stream['video'][0][-2].strip()}...")
+            console.print(f"Downloading {stream['video'][0][0].title}")
+            console.print(f"{stream['video'][0][1]}, {stream['video'][0][-4]}, {stream['video'][0][-2].strip()}...")
             check_stream_existence(stream['video'][0][0], stream['video'][1], stream['video'][2], stream['video'][3])
         
         if 'audio' in stream:
-            print(f"Downloading {stream['audio'][0][1]}, {stream['audio'][0][0].abr}, {stream['audio'][0][-2].strip()}...")
+            console.print(f"Downloading {stream['audio'][0][1]}, {stream['audio'][0][0].abr}, {stream['audio'][0][-2].strip()}...")
             check_stream_existence(stream['audio'][0][0], stream['audio'][1], stream['audio'][2], stream['audio'][3])
         
         if 'video' in stream and 'audio' in stream:
             merge_streams(stream['video'][1], stream['video'][0][-5])
-        print("="*20)
-        print("")
+        console.print(f"[normal1]{'='*20}[/]")
+        console.print("")
