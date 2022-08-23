88dc0b5376410c179244f66a98d4325518dc306a
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2022-08-24 00:46:23 +0200

    Update Video_Metadata.py
    Adding type hints.

--- Video_Metadata.py
+++ Video_Metadata.py
@@ -10,7 +10,7 @@ from rich.table import Table
 console = Console()
 
 
-def grouper_sort(stream_list, group_by=1, sort_by=-1):
+def grouper_sort(stream_list: list, group_by=1, sort_by=-1) -> dict:
     # [<Stream: itag="134" mime_type="video/mp4" res="360p" fps="30fps" vcodec="avc1.4d401e" progressive="False" type="video">, 
     #     'video/mp4', 'video', 'vid_id', '360p', False, '  1.24 GB', 1242.29]
 
@@ -30,9 +30,9 @@ def grouper_sort(stream_list, group_by=1, sort_by=-1):
 
 
 
-def get_vid_metadata(vid_obj, playlist_skip_mime_types = False):
-    # Initially a list but becomes a dict after using grouper_sort()
-    vid_streams_dict = []
+def get_vid_metadata(vid_obj, playlist_skip_mime_types = False) -> dict:
+    # The content is initially stored in a list but eventually is turned into a dict after using grouper_sort()
+    vid_streams_list = []
     
     for stream in vid_obj.streams:
         if playlist_skip_mime_types and stream.mime_type not in ["video/mp4", "audio/mp4"]:
@@ -51,18 +51,18 @@ def get_vid_metadata(vid_obj, playlist_skip_mime_types = False):
                 str_file_size = f"{round(file_size/1000, 2):6} GB"
         else:   str_file_size = f"{file_size:6} MB"
 
-        vid_streams_dict.append([   stream, stream.mime_type, stream.type, vid_obj.video_id,
+        vid_streams_list.append([   stream, stream.mime_type, stream.type, vid_obj.video_id,
                                     stream.resolution, stream.is_progressive, str_file_size, file_size])
 
     # Formatting
-    vid_streams_dict = grouper_sort(vid_streams_dict)
+    vid_streams_dict = grouper_sort(vid_streams_list)
 
     return vid_streams_dict
 
 
 
 # Prints and counts categories and streams
-def print_streams(vid_streams_dict):
+def print_streams(vid_streams_dict: dict) -> list:
     streams_in_each_category = []
 
     table = Table(  style         = "bold blue1", 
@@ -70,7 +70,7 @@ def print_streams(vid_streams_dict):
                     header_style  = "bold deep_pink1",
                     show_lines=True)
     
-    table.add_column("Category", justify="middle", no_wrap=True, vertical="middle")
+    table.add_column("Category", justify="center", no_wrap=True, vertical="middle")
     table.add_column("Quality",  justify='left')
     table.add_column("Size",     justify="right")
     table.add_column("Progressive", justify="left")
